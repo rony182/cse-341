e9adfbbce57f47b0449c627a2d9b965970adb544
rony182 <rony.182@hotmail.com>
2022-09-17 23:09:24 -0300

    almost ready

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router();
 
-routes.get('/', (req, res) => {
-  res.send('Hello, Priscilla! How are you today?');
-});
+// routes.get('/', (req, res) => {
+//   res.send('Hello, Priscilla! How are you today?');
+// });
+routes.use('/contacts', require('./contacts'));
 
 module.exports = routes;
